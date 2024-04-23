2d79f9b1da8e03e6e5337ce7c27b89d45522ac64
darsh18an <120701579+darsh18an@users.noreply.github.com>
2024-04-23 14:50:06 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,83 @@
+console.log("hello world!");
+
+
+
+// se current year
+const yearEl = document.querySelector(".year");
+const currentYear =new Date().getFullYear();
+yearEl.textContent=currentYear; 
+
+//mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+
+btnNavEl.addEventListener('click', function(){
+  headerEl.classList.toggle("nav-open");
+})
+
+
+///////////////////////////////////////////////////////////
+// smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href =link.getAttribute("href");
+    
+
+    // scroll back to top
+    if(href === "#") 
+    window.scrollTo({
+      top: 0,
+      behavior:"smooth",
+    });
+
+
+    // Scroll to other links
+    if(href !== "#" && href.startsWith("#")){
+      const sectionEl = document.queryselector(href);
+       sectionEl.scrollIntoView({behavior:"smooth"});
+     }
+// close mobile navigation
+
+if(link.classList.contains("main-nav-link"))
+headerEl.classList.toggle("nav-open");
+   });
+ 
+
+
+ ///////////////////////////////////////////////////////////
+//  Sticky navigation
+
+const sectionHeroEl =document.querySelector(".section-hero");
+
+const obs =new IntersectionObserver(
+  function(entries){
+const ent = entries[0];
+console.log(ent);
+
+if(ent.isIntersecting === false){
+document.body.classList.add("sticky");
+}
+
+if (ent.isIntersecting === true) {
+  document.body.classList.remove("sticky");
+}
+},
+{
+  // in the viewport
+  root:null,
+  threshold: 0,
+  rootMargin:"-80px",
+}
+);
+obs.observe(sectionHeroEl);
+
+
+
+
+
